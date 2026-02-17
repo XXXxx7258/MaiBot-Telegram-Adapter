cbb9772b4d3114d0f7978ec5865fb494e0cf47ae
XXXxx7258 <3103908461@qq.com>
2026-02-18 03:44:29 +0800

    fix: harden runtime message index for private routing

--- src/runtime_state.py
+++ src/runtime_state.py
@@ -3,26 +3,31 @@ from typing import Optional
 
 
 class TelegramRuntimeState:
+    _MESSAGE_CHAT_INDEX_LIMIT = 20000
+
     def __init__(self) -> None:
         self.bot_id: Optional[int] = None
         self._last_private_chat_id: Optional[int] = None
-        self._message_chat_index: OrderedDict[int, int] = OrderedDict()
-        self._message_chat_index_limit = 20000
+        self._message_chat_index: OrderedDict[tuple[int, int], None] = OrderedDict()
 
     def set_bot_id(self, bot_id: Optional[int]) -> None:
         self.bot_id = bot_id
 
     def record_message(self, chat_id: int, message_id: int, chat_type: str) -> None:
-        self._message_chat_index[message_id] = chat_id
-        self._message_chat_index.move_to_end(message_id)
-        while len(self._message_chat_index) > self._message_chat_index_limit:
+        message_key = (chat_id, message_id)
+        self._message_chat_index[message_key] = None
+        self._message_chat_index.move_to_end(message_key)
+        if len(self._message_chat_index) > self._MESSAGE_CHAT_INDEX_LIMIT:
             self._message_chat_index.popitem(last=False)
 
-        if chat_type not in {"group", "supergroup"}:
+        if chat_type == "private":
             self._last_private_chat_id = chat_id
 
     def get_chat_id_by_message_id(self, message_id: int) -> Optional[int]:
-        return self._message_chat_index.get(message_id)
+        for chat_id, current_message_id in reversed(self._message_chat_index.keys()):
+            if current_message_id == message_id:
+                return chat_id
+        return None
 
     def get_last_private_chat_id(self) -> Optional[int]:
         return self._last_private_chat_id
